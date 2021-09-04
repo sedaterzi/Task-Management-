f12fdef954ba678b887ff39bb5404ca0a9f8e803
sedaterzi <seda_terzi4@hotmail.com>
2021-09-05 00:20:10 +0300

    homePageCreated

---
+++ taskmanagement/lib/homePage.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+
+class TaskPage extends StatefulWidget {
+  const TaskPage({ Key key }) : super(key: key);
+
+  @override
+  _TaskPageState createState() => _TaskPageState();
+}
+
+class _TaskPageState extends State<TaskPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        tooltip: 'Yeni Etkinlik Ekle',
+        backgroundColor: Colors.teal,
+        foregroundColor: Colors.white,
+        child: Icon(Icons.add),
+      ),
+      body:SfCalendar(
+        view:CalendarView.month,
+        firstDayOfWeek: 1,
+        monthViewSettings: MonthViewSettings(
+          appointmentDisplayCount: 2,
+          dayFormat: 'EEEEE',
+          showAgenda: true,
+          agendaViewHeight: 400,
+        ),
+      )
+    );
+  }
+}
