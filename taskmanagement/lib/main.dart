9f42eb8043fcadbf92d46a722d506dd8db9d7fbb
sedaterzi <seda_terzi4@hotmail.com>
2021-09-05 00:36:55 +0300

    local language support

--- taskmanagement/lib/main.dart
+++ taskmanagement/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_localizations/syncfusion_localizations.dart';
 import 'package:taskmanagement/homePage.dart';
 
 void main() {
@@ -9,6 +10,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      //////////Paket Dil Desteği////////////////
+      localizationsDelegates: [SfGlobalLocalizations.delegate],
+      supportedLocales: [
+        const Locale('tr'),
+      ],
+      locale: const Locale('tr'),
+      //////////////////////////
       debugShowCheckedModeBanner: false,
       title: 'Task Management',
       theme: ThemeData(
